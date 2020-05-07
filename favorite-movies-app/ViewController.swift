8f4fafb5ed40cf165f545f619c1cf5d8485042e1
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2020-05-07 23:03:28 +0200

    Initial Commit

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by Krzysztof Wawer on 07/05/2020.
+//  Copyright © 2020 kw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
